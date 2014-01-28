488a8457bbcdd20366c7d8cce0a46b71970944f6
keepcalm444 <keepcalm444@gmail.com>
2014-01-28 03:03:31 -0500

    Move stylesheet into head agin, improve FAQ
    sorry pagespeed :(

--- faq.php
+++ faq.php
@@ -27,6 +27,13 @@ body {
 	width: 100%;
 	text-align: center;
 	position: relative;
+	cursor: pointer;
+/*	-webkit-transition: all 500ms ease;
+	-moz-transition: all 500ms ease;
+	-ms-transition: all 500ms ease;
+	-o-transition: all 500ms ease;
+	transition: all 500ms ease;
+*/
 }
 
 .faq-ans {
@@ -34,40 +41,44 @@ body {
 	font-size: 18px; 
 	margin: 10px; 
 	text-align: left;
-	height: 0;
-}
+/*	-webkit-transition: all 500ms ease;
+	-moz-transition: all 500ms ease;
+	-ms-transition: all 500ms ease;
+	-o-transition: all 500ms ease;
+	transition: all 500ms ease;
+*/}
 </style>
 </head>
 <body>
 <h1>FAQ</h1>
+<a href="/">Back</a>
 <div onclick="toggleHeader('#faq-1')" class="faq" id="faq-1">
 <span style="font-family: Roboto Slab; font-size: 30px; ">About this site</span><br /><br />
-<div id="faq-1-ans" class="faq-ans" style="display: none;">
+<div id="faq-1-ans" class="faq-ans" >
 This site is the spiritual successor to <a href='http://sbhsbelltimes.tk/'>http://sbhsbelltimes.tk</a>
 <br />
 The code can be found on <a href='https://github.com/sbhs-forkbombers/sbhs-timetable'>GitHub</a>
+<br />
+Improvements include:
+<ul>
+<li><strong>Integrated timetable</strong> - on the front page, you can see what class you've got next, what you've got tomorrow, and what the belltimes are for today</li>
+<li><strong>Less annoying animations</strong> - The FAQ button doesn't jiggle like it does on the other website. Neat, huh?</li>
+<li><strong>Intuitive touch-screen support</strong> - Why click when you can swipe? On your phone or tablet, swipe from the left and right of the screen to view more information</li>
 </div>
 </div>
 <div onclick="toggleHeader('#faq-2')" class="faq" id="faq-2">
 <span style="font-family: Roboto Slab; font-size: 30px; ">Why does this site look bad / not work on my computer, but works nicely on others?</span><br /><br />
-<div id="faq-2-ans" class="faq-ans" style="display: none;">
+<div id="faq-2-ans" class="faq-ans" >
 It's probably your computer and web browser. You should switch to the latest version of either <a href="https://www.google.com/chrome">Google Chrome</a> or <a href="https://www.mozilla.org/firefox">Mozilla Firefox</a>. If you're unfortunate and on a DER-loaned laptop and don't want to violate your user / loan charter, you can update to the latest Internet Explorer by clicking "Check online for updates from Windows Update", underneath "Managed by your system administrator" in the Windows Update section of Control Panel.
 <br />
 </div>
 </div>
 <script>
 function toggleHeader(id) {
-	if ($(id).hasClass("expanded")) {
-		$(id + "-ans").css({"height": "0"});
-		$(id + "-ans").css({"display": "none"});
-		$(id).removeClass("expanded");
-	}
-	else {
-		$(id + "-ans").css({"height": "auto"});
-		$(id + "-ans").css({"display": "block"});
-		$(id).addClass("expanded");
-	}
+	$(id+"-ans").slideToggle();
 }
+
+$('.faq-ans').slideUp();
 </script>
 </body>
 </html>
